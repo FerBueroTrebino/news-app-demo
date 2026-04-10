83f8600aba829640f63f394cab246b698539a3c1
FerBueroTrebino <ferbuerotrebino@gmail.com>
2026-04-10 13:28:30 +0200

    Implementation of unit test for presentation layer

---
+++ frontend/test/features/daily_news/presentation/bloc/article/local/local_article_bloc_test.dart
@@ -0,0 +1,101 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_saved_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/remove_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/save_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/local/local_article_event.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/local/local_article_state.dart';
+
+class MockGetSavedArticleUseCase extends Mock
+    implements GetSavedArticleUseCase {}
+
+class MockSaveArticleUseCase extends Mock implements SaveArticleUseCase {}
+
+class MockRemoveArticleUseCase extends Mock implements RemoveArticleUseCase {}
+
+void main() {
+  late LocalArticleBloc bloc;
+  late MockGetSavedArticleUseCase mockGetSavedArticleUseCase;
+  late MockSaveArticleUseCase mockSaveArticleUseCase;
+  late MockRemoveArticleUseCase mockRemoveArticleUseCase;
+
+  setUp(() {
+    mockGetSavedArticleUseCase = MockGetSavedArticleUseCase();
+    mockSaveArticleUseCase = MockSaveArticleUseCase();
+    mockRemoveArticleUseCase = MockRemoveArticleUseCase();
+
+    bloc = LocalArticleBloc(
+      mockGetSavedArticleUseCase,
+      mockSaveArticleUseCase,
+      mockRemoveArticleUseCase,
+    );
+  });
+
+  tearDown(() {
+    bloc.close();
+  });
+
+  const tArticle = ArticleEntity(
+    id: 1,
+    title: 'Test Title',
+    description: 'Test Description',
+  );
+
+  final tArticles = [tArticle];
+
+  test('initial state should be LocalArticlesLoading', () {
+    expect(bloc.state, const LocalArticlesLoading());
+  });
+
+  test('emits [LocalArticlesDone] when GetSavedArticles is added', () async {
+    // arrange
+    when(() => mockGetSavedArticleUseCase()).thenAnswer((_) async => tArticles);
+
+    // assert later
+    final expected = [
+      LocalArticlesDone(tArticles),
+    ];
+
+    expectLater(bloc.stream, emitsInOrder(expected));
+
+    // act
+    bloc.add(const GetSavedArticles());
+  });
+
+  test('emits [LocalArticlesDone] when RemoveArticle is added', () async {
+    // arrange
+    when(() => mockRemoveArticleUseCase(params: tArticle))
+        .thenAnswer((_) async => Future.value());
+    when(() => mockGetSavedArticleUseCase()).thenAnswer((_) async => tArticles);
+
+    // assert later
+    final expected = [
+      LocalArticlesDone(tArticles),
+    ];
+
+    expectLater(bloc.stream, emitsInOrder(expected));
+
+    // act
+    bloc.add(const RemoveArticle(tArticle));
+  });
+
+  test('emits [LocalArticlesDone] when SaveArticle is added', () async {
+    // arrange
+    when(() => mockSaveArticleUseCase(params: tArticle))
+        .thenAnswer((_) async => Future.value());
+    when(() => mockGetSavedArticleUseCase()).thenAnswer((_) async => tArticles);
+
+    // assert later
+    final expected = [
+      LocalArticlesDone(tArticles),
+    ];
+
+    expectLater(bloc.stream, emitsInOrder(expected));
+
+    // act
+    bloc.add(const SaveArticle(tArticle));
+  });
+}
---
+++ frontend/test/features/daily_news/presentation/bloc/article/remote/remote_article_bloc_test.dart
@@ -0,0 +1,74 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+class MockGetArticleUseCase extends Mock implements GetArticleUseCase {}
+
+void main() {
+  late RemoteArticlesBloc bloc;
+  late MockGetArticleUseCase mockGetArticleUseCase;
+
+  setUp(() {
+    mockGetArticleUseCase = MockGetArticleUseCase();
+    bloc = RemoteArticlesBloc(mockGetArticleUseCase);
+  });
+
+  tearDown(() {
+    bloc.close();
+  });
+
+  final tArticles = [
+    const ArticleEntity(
+      id: 1,
+      title: 'Test Title',
+      description: 'Test Description',
+    )
+  ];
+
+  test('initial state should be RemoteArticlesLoading', () {
+    expect(bloc.state, const RemoteArticlesLoading());
+  });
+
+  test(
+      'emits [RemoteArticlesDone] when GetArticles is added and usecase returns DataSuccess',
+      () async {
+    // arrange
+    when(() => mockGetArticleUseCase())
+        .thenAnswer((_) async => DataSuccess(tArticles));
+
+    // assert later -> wait for emits
+    final expected = [
+      RemoteArticlesDone(tArticles),
+    ];
+
+    expectLater(bloc.stream, emitsInOrder(expected));
+
+    // act
+    bloc.add(const GetArticles());
+  });
+
+  test(
+      'emits [RemoteArticlesError] when GetArticles is added and usecase returns DataFailed',
+      () async {
+    // arrange
+    final dioException = DioException(requestOptions: RequestOptions(path: ''));
+    when(() => mockGetArticleUseCase())
+        .thenAnswer((_) async => DataFailed(dioException));
+
+    // assert later -> wait for emits
+    final expected = [
+      RemoteArticlesError(dioException),
+    ];
+
+    expectLater(bloc.stream, emitsInOrder(expected));
+
+    // act
+    bloc.add(const GetArticles());
+  });
+}
